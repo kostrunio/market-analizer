53a45bd1f944f7595e788626eba533ffd3cae040
Michal Kostro <michal.kostro@10g.pl>
2020-04-17 02:05:10 +0200

    better rganized version

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -28,38 +28,37 @@ public class MainView extends VerticalLayout {
         double sellFailure = 0.9;
         double sellSucess = 1.1;
 
-        LocalDate date = LocalDate.of(2020, 01, 01);
+        boolean bought = false;
+        Double price = null;
+        String line = null;
+
+        LocalDate date = LocalDate.of(2020, 03, 18);
         day: for (; date.isBefore(LocalDate.now().plusDays(1)); date = date.plusDays(1)) {
             CandleResponse response = jsonService.getCandles("BTC-PLN", 3600,
                     LocalDateTime.of(2020, date.getMonthValue(), date.getDayOfMonth(), 00, 00, 00),
                     LocalDateTime.of(2020, date.getMonthValue(), date.getDayOfMonth(), 23, 59, 59));
             for (int hour = 0; hour < 23; hour++) {
-                if (response.getLow(hour+1) == null) continue day;
-                if (response.getLow(hour + 1) < response.getClose(hour) * buy) {
-                    Double price = response.getClose(hour) * buy;
-                    String line = LocalDateTime.of(2020, date.getMonthValue(), date.getDayOfMonth(), hour + 1, 00, 00).toString() + ": " + price;
-                    System.out.println(line);
-                    bought:
-                    newDay: for (LocalDate newDate = LocalDate.from(date); newDate.isBefore(LocalDate.now().plusDays(1)); newDate = newDate.plusDays(1)) {
-                        CandleResponse secondResponse = jsonService.getCandles("BTC-PLN", 3600,
-                                LocalDateTime.of(2020, newDate.getMonthValue(), newDate.getDayOfMonth(), 00, 00, 00),
-                                LocalDateTime.of(2020, newDate.getMonthValue(), newDate.getDayOfMonth(), 23, 59, 59));
-                        for (int newHour = newDate.getDayOfMonth() == date.getDayOfMonth() ? hour + 2 : 0; newHour < 23; newHour++) {
-                            if (secondResponse.getLow(newHour) == null) continue newDay;
-                            if (secondResponse.getLow(newHour) < price * sellFailure) {
-                                String newLine = line + " -> " + LocalDateTime.of(2020, newDate.getMonthValue(), newDate.getDayOfMonth(), newHour, 00, 00).toString() + ": " + price * sellFailure + " STRATA";
-                                System.out.println(newLine);
-                                add(new Label(newLine));
-                                result*=sellFailure;
-                                break bought;
-                            } else if (secondResponse.getHigh(newHour) > price * sellSucess) {
-                                String newLine = line + " -> " + LocalDateTime.of(2020, newDate.getMonthValue(), newDate.getDayOfMonth(), newHour, 00, 00).toString() + ": " + price * sellSucess + " ZYSK";
-                                System.out.println(newLine);
-                                add(new Label(newLine));
-                                result*=sellSucess;
-                                break bought;
-                            }
-                        }
+                if (response.getLow(hour + 1) == null) continue day;
+                if (!bought) {
+                    if (response.getLow(hour + 1) < response.getHigh(hour) * buy) {
+                        price = response.getClose(hour) * buy;
+                        line = LocalDateTime.of(2020, date.getMonthValue(), date.getDayOfMonth(), hour + 1, 00, 00).toString() + ": " + price;
+                        System.out.println(line);
+                        bought = true;
+                    }
+                } else {
+                    if (response.getLow(hour + 1) < price * sellFailure) {
+                        String newLine = line + " -> " + LocalDateTime.of(2020, date.getMonthValue(), date.getDayOfMonth(), hour + 1, 00, 00).toString() + ": " + price * sellFailure + " STRATA";
+                        System.out.println(newLine);
+                        add(new Label(newLine));
+                        result *= sellFailure;
+                        bought = false;
+                    } else if (response.getHigh(hour + 1) > price * sellSucess) {
+                        String newLine = line + " -> " + LocalDateTime.of(2020, date.getMonthValue(), date.getDayOfMonth(), hour + 1, 00, 00).toString() + ": " + price * sellSucess + " ZYSK";
+                        System.out.println(newLine);
+                        add(new Label(newLine));
+                        result *= sellSucess;
+                        bought = false;
                     }
                 }
             }
