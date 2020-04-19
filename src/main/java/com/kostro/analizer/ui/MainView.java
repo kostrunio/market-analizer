d75ef8469ed1d03d0d38e9d1c72dad6062e0e621
Michal Kostro <michal.kostro@10g.pl>
2020-04-19 18:09:57 +0200

    generic version

--- src/main/java/com/kostro/analizer/ui/MainView.java
+++ src/main/java/com/kostro/analizer/ui/MainView.java
@@ -2,17 +2,28 @@ package com.kostro.analizer.ui;
 
 import com.kostro.analizer.json.domain.candle.CandleResponse;
 import com.kostro.analizer.json.service.JsonService;
+import com.kostro.analizer.wallet.Configuration;
 import com.kostro.analizer.wallet.Transaction;
 import com.kostro.analizer.wallet.Wallet;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Route("")
 public class MainView extends VerticalLayout {
+
+    double buyFirst = 0.99;
+    double buyLast = 0.90;
+    double sellFailureFirst = 0.99;
+    double sellFailureLast = 0.90;
+    double sellSucessFirst = 1.02;
+    double sellSucessLast = 1.10;
+    double step = 0.01;
+    int wrongLimit = -1;
+    int hours = 1;
 
     private JsonService jsonService;
 
@@ -25,84 +36,49 @@ public class MainView extends VerticalLayout {
     }
 
     private void updateList() {
-        double buyFirst = 0.99;
-        double buyLast = 0.90;
-        double sellFailureFirst = 0.99;
-        double sellFailureLast = 0.90;
-        double sellSucessFirst = 1.02;
-        double sellSucessLast = 1.10;
-        int hours = 1;
+
+        Wallet wallet = new Wallet(1000, 0.9953);
 
         LocalDateTime startDate = LocalDateTime.of(2019, 12, 16, 00, 00, 00);
         LocalDateTime endDate = LocalDateTime.now();
         CandleResponse response = jsonService.getCandles("BTC-PLN", 3600, startDate, endDate);
-
-        /*startDate = LocalDateTime.of(2019, 12, 16, 00, 00, 00);
-        endDate = LocalDateTime.of(2020, 01, 15, 23, 59, 59);
-        System.out.println("buy;sellFailure;sellSucess;wallet;" + startDate.toString() + ';' + endDate.toString());
-        for (double buy = buyFirst; buy >= buyLast; buy -= 0.01)
-            for (double sellFailure = sellFailureFirst; sellFailure >= sellFailureLast; sellFailure -= 0.01)
-                for (double sellSucess = sellSucessFirst; sellSucess <= sellSucessLast; sellSucess += 0.01) {
-                    count(new Wallet(1000, 0.9953), startDate, endDate, response, buy, sellFailure, sellSucess, false);
-        }
-
-        startDate = LocalDateTime.of(2020, 01, 16, 00, 00, 00);
-        endDate = LocalDateTime.of(2020, 02, 15, 23, 59, 59);
-        System.out.println("buy;sellFailure;sellSucess;wallet;" + startDate.toString() + ';' + endDate.toString());
-        for (double buy = buyFirst; buy >= buyLast; buy -= 0.01)
-            for (double sellFailure = sellFailureFirst; sellFailure >= sellFailureLast; sellFailure -= 0.01)
-                for (double sellSucess = sellSucessFirst; sellSucess <= sellSucessLast; sellSucess += 0.01) {
-                    count(new Wallet(1000, 0.9953), startDate, endDate, response, buy, sellFailure, sellSucess, false);
-                }
-
-        startDate = LocalDateTime.of(2020, 02, 16, 00, 00, 00);
-        endDate = LocalDateTime.of(2020, 03, 15, 23, 59, 59);
-        System.out.println("buy;sellFailure;sellSucess;wallet;" + startDate.toString() + ';' + endDate.toString());
-        for (double buy = buyFirst; buy >= buyLast; buy -= 0.01)
-            for (double sellFailure = sellFailureFirst; sellFailure >= sellFailureLast; sellFailure -= 0.01)
-                for (double sellSucess = sellSucessFirst; sellSucess <= sellSucessLast; sellSucess += 0.01) {
-                    count(new Wallet(1000, 0.9953), startDate, endDate, response, buy, sellFailure, sellSucess, false);
-                }
-
-        startDate = LocalDateTime.of(2020, 03, 16, 00, 00, 00);
-        endDate = LocalDateTime.of(2020, 04, 15, 23, 59, 59);
-        System.out.println("buy;sellFailure;sellSucess;wallet;" + startDate.toString() + ';' + endDate.toString());
-        for (double buy = buyFirst; buy >= buyLast; buy -= 0.01)
-            for (double sellFailure = sellFailureFirst; sellFailure >= sellFailureLast; sellFailure -= 0.01)
-                for (double sellSucess = sellSucessFirst; sellSucess <= sellSucessLast; sellSucess += 0.01) {
-                    count(new Wallet(1000, 0.9953), startDate, endDate, response, buy, sellFailure, sellSucess, false);
-                }*/
-
-        Wallet wallet = new Wallet(1000, 0.9953);
-        startDate = LocalDateTime.of(2020, 01, 16, 00, 00, 00);
-        endDate = LocalDateTime.of(2020, 02, 15, 23, 59, 59);
-        System.out.println("buy;sellFailure;sellSucess;wallet;" + startDate.toString() + ';' + endDate.toString());
-        count(wallet, startDate, endDate, response, 0.98, 0.92, 1.05, false);
-
-        startDate = LocalDateTime.of(2020, 02, 16, 00, 00, 00);
-        endDate = LocalDateTime.of(2020, 03, 15, 23, 59, 59);
-        System.out.println("buy;sellFailure;sellSucess;wallet;" + startDate.toString() + ';' + endDate.toString());
-        count(wallet, startDate, endDate, response, 0.97, 0.94, 1.1, false);
-
-        startDate = LocalDateTime.of(2020, 03, 16, 00, 00, 00);
-        endDate = LocalDateTime.of(2020, 04, 15, 23, 59, 59);
-        System.out.println("buy;sellFailure;sellSucess;wallet;" + startDate.toString() + ';' + endDate.toString());
-        count(wallet, startDate, endDate, response, 0.93, 0.97, 1.09, false);
-
-        startDate = LocalDateTime.of(2020, 04, 16, 00, 00, 00);
-        endDate = LocalDateTime.now();
-        System.out.println("buy;sellFailure;sellSucess;wallet;" + startDate.toString() + ';' + endDate.toString());
-        count(wallet, startDate, endDate, response, 0.95, 0.92, 1.09, true);
+        //startDate
+        endDate = LocalDateTime.now().isAfter(startDate.plusMonths(1).minusSeconds(1)) ? startDate.plusMonths(1).minusSeconds(1) : LocalDateTime.now();
+        do {
+            Configuration configuration = countConfiguration(startDate, endDate, response);
+            System.out.println(configuration);
+            startDate = startDate.plusMonths(1);
+            endDate = LocalDateTime.now().isAfter(startDate.plusMonths(1).minusSeconds(1)) ? startDate.plusMonths(1).minusSeconds(1) : LocalDateTime.now();
+            count(wallet, startDate, endDate, response, configuration.getBuy(), configuration.getSellFailure(), configuration.getSellSuccess(), true, wrongLimit);
+        } while(startDate.plusMonths(1).isBefore(LocalDateTime.now()));
     }
 
-    private void count(Wallet wallet, LocalDateTime startDate, LocalDateTime endDate, CandleResponse response, double buy, double sellFailure, double sellSucess, boolean withDetails) {
+    private Configuration countConfiguration(LocalDateTime startDate, LocalDateTime endDate, CandleResponse response) {
+//        System.out.println("buy;sellFailure;sellSucess;wallet;" + startDate.toString() + ';' + endDate.toString());
+        List<Configuration> configurationList = new ArrayList<>();
+        for (double buy = buyFirst; buy >= buyLast; buy -= step)
+            for (double sellFailure = sellFailureFirst; sellFailure >= sellFailureLast; sellFailure -= step)
+                for (double sellSucess = sellSucessFirst; sellSucess <= sellSucessLast; sellSucess += step)
+                    configurationList.add(count(new Wallet(1000, 0.9953), startDate, endDate, response, buy, sellFailure, sellSucess, false, -1));
+        return findConfiguration(configurationList);
+    }
+
+    private Configuration findConfiguration(List<Configuration> configurationList) {
+        Configuration toReturn = new Configuration(0, 0, 0, 0);
+        for (Configuration configuration : configurationList)
+            if (configuration.getResult() > toReturn.getResult())
+                toReturn = configuration;
+            return toReturn;
+    }
+
+    private Configuration count(Wallet wallet, LocalDateTime startDate, LocalDateTime endDate, CandleResponse response, double buy, double sellFailure, double sellSucess, boolean withDetails, int wrongLimit) {
         double lastPrice = 0;
         int wrong = 0;
         hour:for (LocalDateTime date = LocalDateTime.from(startDate); date.isBefore(endDate); date = date.plusHours(1)) {
             if (response.getLow(date) == null || response.getLow(date.plusHours(1)) == null) continue hour;
             lastPrice = response.getLow(date.plusHours(1));
             if (wallet.hasMoney()) {
-                if (wrong < 3)
+                if (wrongLimit > 0 && wrong > wrongLimit) break;
                 if (response.getLow(date.plusHours(1)) < response.getHigh(date) * buy) {
                     wallet.buy(date.plusHours(1), response.getHigh(date) * buy);
                 }
@@ -116,10 +92,11 @@ public class MainView extends VerticalLayout {
                 }
             }
         }
-//        if (!wallet.hasMoney()) wallet.sell(endDate, lastPrice);
+        if (withDetails) {
         System.out.println(buy + ";" + sellFailure + ";" + sellSucess + ";" + (wallet.getMoney() + wallet.getBitcoin()*lastPrice));
-        if (withDetails)
             for (Transaction transaction : wallet.getTransactionHistory())
                 System.out.println(transaction);
+        }
+        return new Configuration(buy, sellFailure, sellSucess, wallet.getMoney() + wallet.getBitcoin()*lastPrice);
     }
 }
---
+++ src/main/java/com/kostro/analizer/wallet/Configuration.java
@@ -0,0 +1,41 @@
+package com.kostro.analizer.wallet;
+
+public class Configuration {
+    private double buy;
+    private double sellFailure;
+    private double sellSuccess;
+    private double result;
+
+    public Configuration(double buy, double sellFailure, double sellSuccess, double result) {
+        this.buy = buy;
+        this.sellFailure = sellFailure;
+        this.sellSuccess = sellSuccess;
+        this.result = result;
+    }
+
+    public double getBuy() {
+        return buy;
+    }
+
+    public double getSellFailure() {
+        return sellFailure;
+    }
+
+    public double getSellSuccess() {
+        return sellSuccess;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Configuration{" +
+                "buy=" + buy +
+                ", sellFailure=" + sellFailure +
+                ", sellSuccess=" + sellSuccess +
+                ", result=" + result +
+                '}';
+    }
+}
