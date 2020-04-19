1a826d924e0a8c11a930ac22ee08afb6b0bf4cfe
Michal Kostro <michal.kostro@10g.pl>
2020-04-19 14:23:49 +0200

    reorginized version

--- src/main/java/com/kostro/analizer/ui/MainView.java
+++ src/main/java/com/kostro/analizer/ui/MainView.java
@@ -24,39 +24,50 @@ public class MainView extends VerticalLayout {
     }
 
     private void updateList() {
-        Wallet wallet = new Wallet(1000, 0.9953);
-        double buy = 0.95;
-        double sellFailure = 0.9;
-        double sellSucess = 1.1;
-
-        String line = null;
+        double buyFirst = 0.99;
+        double buyLast = 0.90;
+        double sellFailureFirst = 0.99;
+        double sellFailureLast = 0.90;
+        double sellSucessFirst = 1.02;
+        double sellSucessLast = 1.10;
+        int hours = 1;
 
         LocalDateTime startDate = LocalDateTime.of(2020, 03, 18, 00, 00, 00);
-        LocalDateTime now = LocalDateTime.now();
-        CandleResponse response = jsonService.getCandles("BTC-PLN", 3600, startDate, now);
-        hour: for (LocalDateTime date = LocalDateTime.from(startDate); date.isBefore(now); date = date.plusHours(1)) {
+        LocalDateTime endDate = LocalDateTime.now();
+        CandleResponse response = jsonService.getCandles("BTC-PLN", 3600, startDate, endDate);
+
+        System.out.println("buy;sellFailure;sellSucess;wallet");
+
+        /*for (double buy = buyFirst; buy > buyLast; buy -= 0.01)
+            for (double sellFailure = sellFailureFirst; sellFailure > sellFailureLast; sellFailure -= 0.01)
+                for (double sellSucess = sellSucessFirst; sellSucess < sellSucessLast; sellSucess += 0.01) {
+                    count(new Wallet(1000, 0.9953), startDate, endDate, response, buy, sellFailure, sellSucess);
+        }*/
+
+        count(new Wallet(1000, 0.9953), startDate, endDate, response, 0.97, 0.94, 1.09);
+    }
+
+    private void count(Wallet wallet, LocalDateTime startDate, LocalDateTime endDate, CandleResponse response, double buy, double sellFailure, double sellSucess) {
+        double lastPrice = 0;
+        hour:for (LocalDateTime date = LocalDateTime.from(startDate); date.isBefore(endDate); date = date.plusHours(1)) {
             if (response.getLow(date) == null || response.getLow(date.plusHours(1)) == null) continue hour;
+            lastPrice = response.getLow(date);
             if (wallet.hasMoney()) {
                 if (response.getLow(date.plusHours(1)) < response.getHigh(date) * buy) {
                     wallet.buy(response.getHigh(date) * buy);
-                    line = date.plusHours(1).toString() + ": " + wallet.getPrice();
-                    System.out.println(line);
                 }
             } else {
                 if (response.getLow(date.plusHours(1)) < wallet.getPrice() * sellFailure) {
                     wallet.sell(wallet.getPrice() * sellFailure);
-                    String newLine = line + " -> " + date.plusHours(1).toString() + ": " + wallet.getPrice() + " STRATA";
-                    System.out.println(newLine);
-                    add(new Label(newLine));
                 } else if (response.getHigh(date.plusHours(1)) > wallet.getPrice() * sellSucess) {
                     wallet.sell(wallet.getPrice() * sellSucess);
-                    String newLine = line + " -> " + date.plusHours(1).toString() + ": " + wallet.getPrice() + " ZYSK";
-                    System.out.println(newLine);
-                    add(new Label(newLine));
 
                 }
             }
         }
-        System.out.println("result: " + wallet.getMoney() + " PLN + " + wallet.getBitcoin() + "BTC");
+        if (!wallet.hasMoney()) wallet.sell(lastPrice);
+//        if (wallet.getMoney() > 1100)
+
+            System.out.println(buy + ";" + sellFailure + ";" + sellSucess + ";" + wallet.getMoney());
     }
 }
